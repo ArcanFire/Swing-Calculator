ec2d36af8ccd81e0f264735f3089e213cd3758ba
Glen Ward <arcangw@gmail.com>
2021-02-24 15:06:40 +1300

    Added base code to file.

---
+++ src/Calculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import javax.swing.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("This is a 2 value calculator.");
+        Scanner calcScanner = new Scanner(System.in);   //setup System.in stream
+        float calcNum1 = 0;
+        float calcNum2 = 0;
+        char operator;  //setup variables for use, set to float for decimal numbers
+//        System.out.println("Enter first number:");
+//        calcNum1 = calcScanner.nextFloat();
+//        System.out.println("Enter operator:");
+//        operator = calcScanner.next().charAt(0);
+//        System.out.println("Enter second number:");
+//        calcNum2 = calcScanner.nextFloat();
+//        switch(operator){
+//            case '+':
+//                System.out.println("Answer is " + (calcNum1 + calcNum2));
+//                break;
+//            case '-':
+//                System.out.println("Answer is " + (calcNum1 + calcNum2));
+//                break;
+//            case '*':
+//                System.out.println("Answer is " + (calcNum1 + calcNum2));
+//                break;
+//            case '/':
+//                System.out.println("Answer is " + (calcNum1 + calcNum2));
+//                break;
+//        }
+        JFrame calcFrame = new JFrame("Calculator");
+        calcFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calcFrame.setSize(300,300);
+        JButton calcOne = new JButton("1");
+        JButton calcTwo = new JButton("2");
+        JButton calcThree = new JButton("3");
+        JButton calcFour = new JButton("4");
+        JButton calcFive = new JButton("5");
+        JButton calcSix = new JButton("6");
+        JButton calcSeven = new JButton("7");
+        JButton calcEight = new JButton("8");
+        JButton calcNine = new JButton("9");
+        calcFrame.getContentPane().add(calcOne); // Adds Button to content pane of frame
+        calcFrame.getContentPane().add(calcTwo);
+        calcFrame.getContentPane().add(calcThree);
+        calcFrame.getContentPane().add(calcFour);
+        calcFrame.getContentPane().add(calcFive);
+        calcFrame.getContentPane().add(calcSix);
+        calcFrame.getContentPane().add(calcSeven);
+        calcFrame.getContentPane().add(calcEight);
+        calcFrame.getContentPane().add(calcNine);
+
+        calcFrame.setVisible(true);
+    }
+}
