9fda3b8c34341f13217bba97c944dce1d489b118
Glen Ward <arcangw@gmail.com>
2021-02-24 15:19:55 +1300

    Testing swing button layouts.

--- src/Calculator.java
+++ src/Calculator.java
@@ -28,28 +28,41 @@ public class Calculator {
 //                System.out.println("Answer is " + (calcNum1 + calcNum2));
 //                break;
 //        }
-        JFrame calcFrame = new JFrame("Calculator");
-        calcFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        calcFrame.setSize(300,300);
-        JButton calcOne = new JButton("1");
-        JButton calcTwo = new JButton("2");
-        JButton calcThree = new JButton("3");
-        JButton calcFour = new JButton("4");
-        JButton calcFive = new JButton("5");
-        JButton calcSix = new JButton("6");
-        JButton calcSeven = new JButton("7");
-        JButton calcEight = new JButton("8");
-        JButton calcNine = new JButton("9");
-        calcFrame.getContentPane().add(calcOne); // Adds Button to content pane of frame
-        calcFrame.getContentPane().add(calcTwo);
-        calcFrame.getContentPane().add(calcThree);
-        calcFrame.getContentPane().add(calcFour);
-        calcFrame.getContentPane().add(calcFive);
-        calcFrame.getContentPane().add(calcSix);
-        calcFrame.getContentPane().add(calcSeven);
-        calcFrame.getContentPane().add(calcEight);
-        calcFrame.getContentPane().add(calcNine);
+//        JFrame calcFrame = new JFrame("Calculator");
+//        calcFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        calcFrame.setSize(300,300);
+//        JButton calcOne = new JButton("1");
+//        JButton calcTwo = new JButton("2");
+//        JButton calcThree = new JButton("3");
+//        JButton calcFour = new JButton("4");
+//        JButton calcFive = new JButton("5");
+//        JButton calcSix = new JButton("6");
+//        JButton calcSeven = new JButton("7");
+//        JButton calcEight = new JButton("8");
+//        JButton calcNine = new JButton("9");
+//        calcFrame.getContentPane().add(calcOne); // Adds Button to content pane of frame
+//        calcFrame.getContentPane().add(calcTwo);
+//        calcFrame.getContentPane().add(calcThree);
+//        calcFrame.getContentPane().add(calcFour);
+//        calcFrame.getContentPane().add(calcFive);
+//        calcFrame.getContentPane().add(calcSix);
+//        calcFrame.getContentPane().add(calcSeven);
+//        calcFrame.getContentPane().add(calcEight);
+//        calcFrame.getContentPane().add(calcNine);
+//
+//        calcFrame.setVisible(true);
 
-        calcFrame.setVisible(true);
+        JFrame f=new JFrame();//creating instance of JFrame
+
+        JButton b=new JButton("click");//creating instance of JButton
+        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+        JButton c = new JButton("test");
+
+        f.add(b);//adding button in JFrame
+        f.add(c);
+
+        f.setSize(400,500);//400 width and 500 height
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
     }
 }
